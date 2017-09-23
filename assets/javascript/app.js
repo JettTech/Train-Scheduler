5bc31bbfc88f142151e56172baa10b2dbcd37f74
Lisa Jetton <ldjetton21@gmail.com>
2017-09-23 04:53:27 -0500

    js file added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,127 @@
 $(document).ready(function($) {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBdDMt-PKYOgmpz4xIieOU17nx9l2eNRTQ",
-    authDomain: "train-scheduler-a47fb.firebaseapp.com",
-    databaseURL: "https://train-scheduler-a47fb.firebaseio.com",
-    projectId: "train-scheduler-a47fb",
-    storageBucket: "",
-    messagingSenderId: "910917476946"
-  };
-  firebase.initializeApp(config);
+// Initializing Firebase //IF having this on the main thread is deprecated... what should we do instead/how should we structure it?!
+	var config = {
+	    apiKey: "AIzaSyBdDMt-PKYOgmpz4xIieOU17nx9l2eNRTQ",
+	    authDomain: "train-scheduler-a47fb.firebaseapp.com",
+	    databaseURL: "https://train-scheduler-a47fb.firebaseio.com",
+	    projectId: "train-scheduler-a47fb",
+	    storageBucket: "",
+	    messagingSenderId: "910917476946"
+  	};
+  	firebase.initializeApp(config);
+
+  	var dataRef = firebase.database();
+
+	var clearButton = $("<button>").addClass("clear");
+	clearButton.attr("type", "submit");
+	clearButton.text("Refresh");
+
+	//To enumberate train entry (by count).
+  	var entryAmount = 0;
+
+//Event Handler --> Whenever button pushed, data submitted/stored in firebase database!
+	$("#addTrainButton").on("click", function() {
+		event.preventDefault();
+
+		$("mainHeader").append(clearButton);
+	
+	////Keeping count of the entries, internally.
+		entryAmount ++;
+		console.log(entryAmount);
+
+	////grabbing all data entries
+		var trainName = $("#trainName-input").val().trim();
+		var destination = $("#destination-input").val().trim();
+		
+		var firstTrainT = moment($("#firstTimeMil-input").val().trim()).format("HH:mm"); //Moment.js REFERENCE: moment().format("ddd, hh");    
+		console.log(firstTrainT);
+		var trainFreq = moment($("#frequency-input").val().trim()).minute(); //Moment.js REFERENCE: moment().minute(Number)
+		console.log(trainFreq);
 
 
+	//Moment.js VARs!!!! - Calculating additional Time Mechs
+		var nextTrn = moment(firstTrainT - trainFreq).format("HH:mm"); //Moment.js REFERENCE: moment().format("ddd, hh")
+		var minAway = moment().set('minute', firstTrainT - nextTrn); //Moment.js REFERENCE: moment().set('minute', 20);
+
+	////storing all data entires AS ONE OBJECT (for firebase database)
+		var newTrain = {
+			num: entryAmount,
+			name: trainName,
+			dest: destination,
+			firstTrn: firstTrainT,
+			freq: trainFreq,
+			next: nextTrn,
+			distance: minAway,
+		};
+
+	//UPLOAD to FireBase!!!!
+		database.ref().push(newTrain);
+		console.log("Train " + entryAmount + "added to database.") //How can we store data taht itemized which train # this is...?
+
+	//// BUG-CHECK: verify data-entry
+		console.log(newtrain.num);
+		console.log(newTrain.name);
+		console.log(newTrain.dest);
+		console.log(newTrain.firstTrn);
+		console.log(newTrain.freq);
+		console.log(newTrain.next)
+		console.log(newTrain.distance)
+
+	// Clear out the TRAIN INPUT form area
+		$("#trainName-input").val("");
+		$("#destination-input").val("");
+		$("#firstTimeMil-input").val("");
+		$("#frequency-input").val("");	
+	});
+
+//Event Handler --> Whenever data submitted and uploaded into FireBase, find eah value, consolelot it and place in into the first display table (area).
+	dataRef.ref().on("child_added", function(childSnapshot, prevChildKey) { ///REVIEW what the "prevChildKey" accomplishes....
+		event.preventDefault();
+
+		//Because the data entry poitns are stored as Separate VALUES on an Obect ( the one we created earlier), we must esablish a new set of vars to be used for tefercing these indivudal values
+		var entryAmount = childSnapshot.val().num;
+		var trainName = childSnapshot.val().name;
+		var destination = childSnapshot.val().dest;
+		var firstTrainT = childSnapshot.val().firstTrn;
+		var trainFreq = childSnapshot.val().freq;
+		var nextTrn = childSnapshot.val().next;
+		var minAway = childSnapshot.val().distance;
+
+		console.log(entryAmount);
+		console.log(trainName);
+		console.log(destination);
+		console.log(firstTrainT);
+		console.log(trainFreq);
+		console.log(nextTrn);
+		console.log(minAway);
+
+		// $("#trainT >tbody").append("<tr><td>" + entryAmount + "")
+		$("#num").append(entryAmount);
+		$("#name").append(trainName);
+		$("#dest").append(destination);
+		$("#freq").append(trainFreq);
+		$("#firstTrn").append(nextTrn);
+		$("#nextTrn").append(minAway);		
+
+	}, function(errorObject){
+		console.log("Error to handle: " + errorObject.code);
+	});
+
+	$(document).on("click", clearButton, function(){
+	// Clear out the TRAIN DISPLAY form AREA		
+		$("#num").val("");
+		$("#name").val("");
+		$("#dest").val("");
+		$("#freq").val("");
+		$("#firstTrn").val("");
+		$("#nextTrn").val("");
+
+	// Clear out the TRAIN INPUT form area
+		$("#trainName-input").val("");
+		$("#destination-input").val("");
+		$("#firstTimeMil-input").val("");
+		$("#frequency-input").val("");	
+	});
 
 }); //END OF JS FILE
