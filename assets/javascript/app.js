ee4f0d9348b496d67d2b7ae49a9a9fd0925a4c34
Lisa Jetton <ldjetton21@gmail.com>
2017-09-23 01:28:47 -0500

    updated firebase link

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function($) {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdDMt-PKYOgmpz4xIieOU17nx9l2eNRTQ",
+    authDomain: "train-scheduler-a47fb.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a47fb.firebaseio.com",
+    projectId: "train-scheduler-a47fb",
+    storageBucket: "",
+    messagingSenderId: "910917476946"
+  };
+  firebase.initializeApp(config);
+
+
+
+}); //END OF JS FILE
